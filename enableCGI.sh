c57f48cc1a3051303be11a7c750ed6fe9b62653e
Christoph Berg <christoph.berg@credativ.de>
2016-11-07 15:59:02 +0100

    Update enableCGI.sh

--- enableCGI.sh
+++ enableCGI.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#Simple script to enable CGI on Debian via APACHE
+# Enable CGI on Debian via Apache
 
+set -e
+
+if ! test -f omdb.cgi; then
+	echo "Could not find omdb.cgi!"
+	echo "Make sure it's within PWD"
+	exit 1
+fi
 
 apt-get install apache2
 a2enmod cgi
 
-test -f omdb.cgi
-if [ $? -ne 0 ]; then 
-	echo "Could not find omdb.cgi!"
-	echo "Make sure it's within PWD"
-	exit 1;
-fi
+rm -f /usr/lib/cgi-bin/omdb
+ln -s $PWD/omdb.cgi /usr/lib/cgi-bin/omdb
 
-ln -s `pwd`/omdb.cgi /usr/lib/cgi-bin/omdb
+sed -i -e 's/SymLinksIfOwnerMatch/FollowSymLinks/g' /etc/apache2/conf-available/serve-cgi-bin.conf
 
-cat /etc/apache2/conf-available/serve-cgi-bin.conf | sed s/SymLinksIfOwnerMatch/FollowSymLinks/g > /etc/apache2/conf-available/serve-cgi-bin.conf.new
-mv /etc/apache2/conf-available/serve-cgi-bin.conf.new /etc/apache2/conf-available/serve-cgi-bin.conf
-
-/etc/init.d/apache2 restart
+service apache2 reload
