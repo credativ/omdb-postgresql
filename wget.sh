617e24f82a2c89a95f2436fdeef9a3ada4c53e66
Christoph Berg <christoph.berg@credativ.de>
2018-05-16 11:26:53 +0200

    Remove username/password from wget.sh

--- wget.sh
+++ wget.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# simple OMDB benchmark
+
+# if localhost needs username/password, put it in ~/.netrc:
+# machine localhost login admin password admin
+
+# clean up downloaded files
 rm -rf localhost
 trap "rm -rf localhost" 0 2 3 15
 
-wget --no-check-certificate --auth-no-challenge -r -np -nv https://admin:admin@localhost/cgi-bin/omdb/
+wget --no-check-certificate --auth-no-challenge -r -np -nv https://localhost/cgi-bin/omdb/
