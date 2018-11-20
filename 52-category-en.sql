d92f2af71a084f938b31fcf6ea380cf007eba0a5
Christoph Berg <christoph.berg@credativ.de>
2018-11-20 12:07:33 +0100

    Some categories do not have English names, fall back to German

--- 52-category-en.sql
+++ 52-category-en.sql
@@ -4,3 +4,9 @@ UPDATE categories c
   SET name = n.name
   FROM category_names n
   WHERE c.id = n.category_id AND n.language = 'en';
+
+-- Some categories do not have English names, fall back to German
+UPDATE categories c
+  SET name = n.name
+  FROM category_names n
+  WHERE c.id = n.category_id AND n.language = 'de' AND c.name IS NULL;
