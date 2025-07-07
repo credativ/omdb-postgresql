01dccdeefa5eca77c5c7fe22e7b6bbd9aaec9eb7
Michael Banck <michael.banck@credativ.de>
2025-07-07 09:32:36 +0200

    Remove entries from people_links with missing person_id

--- 40-clean-orphans.sql
+++ 40-clean-orphans.sql
@@ -3,6 +3,7 @@ BEGIN;
 UPDATE movies c SET parent_id = NULL WHERE NOT EXISTS (SELECT * FROM movies p WHERE p.id = c.parent_id);
 
 DELETE FROM people_aliases     c WHERE NOT EXISTS (SELECT * FROM people p     WHERE p.id = c.person_id);
+DELETE FROM people_links       c WHERE NOT EXISTS (SELECT * FROM people p     WHERE p.id = c.person_id);
 DELETE FROM casts              c WHERE NOT EXISTS (SELECT * FROM movies p     WHERE p.id = c.movie_id);
 DELETE FROM movie_categories   c WHERE NOT EXISTS (SELECT * FROM movies p     WHERE p.id = c.movie_id);
 DELETE FROM movie_categories   c WHERE NOT EXISTS (SELECT * FROM categories p WHERE p.id = c.category_id);
